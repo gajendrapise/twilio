2235186bd07ffb3bcd03a3bfecc724305e2b5a61
gajendrapise <97894108+gajendrapise@users.noreply.github.com>
2022-04-08 12:21:34 +0530

    new file

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Twilio Website Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <style>
+  .fakeimg {
+    height: 200px;
+    background: #aaa;
+  }
+  </style>
+</head>
+<body>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Twilio Docs</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">SMS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Voice</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Runtime</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Vidio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Studio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">All docs</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-sm-3">
+      <h3 class="mt-4">Get started</h3>
+      <p>Use your free twilio account</p>
+      <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Sign up for your free Twilio trial account</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Verify your persnol phone number</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Get your first twilio phone number</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Trial account restriction and limitation</a>
+        </li>
+      </ul>
+      <hr class="d-sm-none">
+    </div>
+    <div class="col-sm-9">
+      <h2>
+How to Work with your Free Twilio Trial Account
+</h2>
+<p>So you want to give Twilio a spin, or you’ve already signed up for your trial account and want to understand what you can and can’t do? You’re in the right place!</p>
+
+      <h3 class="mt-5">Sign up for your free Twilio trial</h3>
+      <p>Click the Phone Numbers dropdown on the side navigation menu:</p>
+          <img src="css/pn.png" alt="myimage" width="50%"> 
+<p>Click Verified caller IDs under the Manage dropdown. Once on the Verified caller IDs page, click the Add a new Caller ID button to add a new number:</p>
+          <img src="css/vi.png" alt="myimage" width="100%">
+          <p>Enter the desired phone number to verify, select the desired verification method, and then click Verify Number: </p> 
+          <img src="css/ai.png" alt="myimage" width="70%">
+         <p> Note You will need access to this device to receive the call or text with your verification code. </p>
+         <p>Enter the verification code. You’re now ready to text or call this number with your trial Twilio account. </p>
+          <img src="css/as.png" alt="myimage" width="70%">
+          <h3>Get your first Twilio phone number</h3>
+          <p>You will need a Twilio phone number — a phone number purchased through Twilio — to send messages or make phone calls using Twilio.
+
+After signing up for your trial account, navigate to the Phone Numbers page in your console. Click Buy a Number to purchase your first Twilio number.</p> 
+          <img src="css/bn.png" alt="myimage" width="100%">
+          <p>On the "Buy a Number" page you'll see a list of all available numbers as well as their capabilities (Voice, SMS, MMS, and Fax). You can customize the country and area code, as well as select the any of the capabilities you'd like this number to have. When you find the one that meets your needs click Buy to review the number.</p> 
+          <img src="css/rn.png" alt="myimage" width="60%"> 
+          <p>
+
+If you don’t use your phone number for more than 30 days, we’ll remove it from your trial project. If you decide to return, you can always pick a new phone number.
+
+
+If you choose to upgrade your project, your trial number will be carried over and charged monthly according to current Voice/SMS rates.
+
+</p>
+<h3>How to release a Twilio phone number</h3>
+<p>We only allow one Twilio phone number per trial project. If you select a number or choose your own and decide that you’d like a different phone number, you must first release your current number, then pick a new number from the Buy a Number page.
+
+To release your current Twilio phone number:</p>
+          <img src="css/mn.png" alt="myimage" width="50%"> 
+
+    </div>
+  </div>
+</div>
+
+<div class="mt-5 p-4 bg-dark text-white text-center">
+  <p>Footer</p>
+</div>
+
+</body>
+</html>
